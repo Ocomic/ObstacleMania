8bcdfa4a7f2d5a5fc7a07a43fc0d153f73f0af4f
Ocomic <108867917+Ocomic@users.noreply.github.com>
2022-11-28 23:24:01 +0100

    implemented movement
    - added function to move player
    - added walls for collisions
    - added virtual camera with brain

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,19 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
-{          
+{
+    [SerializeField] float moveSpeed = 5f;          
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
-        
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the Game");
+        Debug.Log("WASD to move around");
+        Debug.Log("avoid all obstacles");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
         transform.Translate(xValue, 0, zValue);
     }
+
 }
